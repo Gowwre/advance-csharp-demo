e81d6e3df86b6182453e2a6435300b11ca79c0a5
pytas0811 <tekato2002@gmail.com>
2023-10-17 20:24:19 +0700

    Add Concurrency thread

--- Concurrency/Program.cs
+++ Concurrency/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using static System.Console;
 
-Console.WriteLine("Hello, World!");
+
+static void DoWork()
+{
+    WriteLine("Doing the work...");
+    Thread.Sleep(1000);
+    WriteLine("done");
+}
+
+var watch = Stopwatch.StartNew();
+
+var t1 = new Thread(DoWork);
+var t2 = new Thread(DoWork);
+
+
+// start both threads
+t1.Start();
+t2.Start();
+
+// wait for both threads completed
+t1.Join();
+t2.Join();
+
+
+watch.Stop();
+WriteLine($"It took {watch.Elapsed.Seconds} second(s) to complete.");
